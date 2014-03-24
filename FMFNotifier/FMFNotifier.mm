9b22d21153e23f63f0fc61c42660558ea2644926
Gianluca Puglia <Gianluca Puglia>
2014-03-24 23:30:19 +0100

    Fix

--- FMFNotifier/FMFNotifier.mm
+++ FMFNotifier/FMFNotifier.mm
@@ -6,10 +6,6 @@
 //  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
 //
 
-/* TODO:
- * dylib traslations
- */
-
 #import "CaptainHook/CaptainHook.h"
 #import "KeychainItemWrapper.h"
 #import "Notifier.h"
